20032032d49f875fc07d0f0ff409bf9d7a89f262
Jesus Vazquez <jesusvb314@gmail.com>
2025-08-07 16:08:55 -0600

    Solucion Propuesta

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,40 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Arreglo global para guardar nombres
+let arregloAmigos = [];
+
+/*
+Funcion que obtiene el valor del input evalua si es vacio 
+y de ser valido lo agrega al arreglo 
+*/
+function agregarAmigo() {
+  let nombreAmigo = document.getElementById("amigo").value;
+
+  if (!nombreAmigo == "") {
+    arregloAmigos.push(nombreAmigo);
+    document.getElementById("listaAmigos").innerHTML =
+      recorrerLista(arregloAmigos);
+    document.getElementById("amigo").value = "";
+  } else {
+    alert("El campo no puede ser vacio");
+  }
+}
+
+/*
+Funcion que actualiza el Doom
+*/
+function recorrerLista(arregloAmigos) {
+  let amigostxt = "";
+  for (i = 0; i < arregloAmigos.length; i++) {
+    amigostxt = amigostxt + `<li>${arregloAmigos[i]}</li>`;
+  }
+  return amigostxt;
+}
+
+/** 
+ Generar numero aleatorio en el rango del arreglo
+*/
+function sortearAmigo() {
+  
+  let numeroAleatoreo = Math.floor(Math.random() * arregloAmigos.length);
+  let amigoSecreto = arregloAmigos[numeroAleatoreo];
+  document.getElementById("resultado").innerHTML = `<li>${amigoSecreto}</li>`;
+}
